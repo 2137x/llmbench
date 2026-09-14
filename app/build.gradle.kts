6e553066273f8ffd0799267d58e03795cb2febe5
trvny <120686325+trvny@users.noreply.github.com>
2026-09-14 18:42:55 +0200

    Target Android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     defaultConfig {
         applicationId = "ais.tee"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
